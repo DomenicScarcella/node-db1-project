0ca21bb153c33d8e7bef4ed85e4daf5e4ef59e1a
Domenic Scarcella <ls41513@yahoo.com>
2021-04-12 22:21:59 -0400

    Almost nothing yet on actual tests

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,11 +1,33 @@
+const Acc = require('./accounts-model.js');
+
 exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
+  const name = req.body.name;
+  const budget = req.body.budget;
+  if (!name || !budget) {
+    res.status(400).json({message: "name and budget are required"})
+  } else if (typeof name != 'string') {
+    res.status(400).json({message: "name of account must be a string"})
+  } else if (name.trim().length() < 3 || name.trim().length() > 100) {
+    res.status(400).json({message: "name of account must be between 3 and 100"})
+  } else if (typeof budget != 'number') {
+    res.status(400).json({message: "budget of account must be a number"})
+  } else if (budget < 0 || budget > 1000000) {
+    res.status(400).json({message: "budget of account is too large or too small"})
+  } else {
+    next()
+  }
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
   // DO YOUR MAGIC
+
 }
 
-exports.checkAccountId = (req, res, next) => {
+exports.checkAccountId = async (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const account = await Acc.get(req.params.id);
+    if 
+  } catch
 }
